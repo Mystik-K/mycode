1947da2a0b75e6419018107708251b38585d6b5c
Mystik-K <mkoprucu@hawaii.edu>
2023-01-03 23:19:15 +0000

    join and split

---
+++ lab13.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+#input
+name = input("What is your name? ")
+
+print("")
+#input
+dayofweek = input("What is the day of the week? ")
+
+
+print ("Hello ", + name + "!", + "\nHappy ", + dayofweek + "!")
